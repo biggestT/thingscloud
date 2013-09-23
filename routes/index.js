772619c272171cc565c4c140eb0372705b897f95
Tor <tornilssonohrn@gmail.com>
2013-09-23 18:03:17 +0200

    fixed neo4j connection

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@
 var request = require('superagent');
 
 exports.index = function(req, res) {
-	request.post(process.en.NEO_REST_URL + 
+	request.post(process.en.NEO4J_REST_URL + 
 		'/cypher').send({
 			query: 'START n=node(1) RETURN n.title'
 		}).end(function (neo4jRes){
