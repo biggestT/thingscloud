01a75c7aeeb5ac8f33ea243abcff6a9f4b206484
Tor <tornilssonohrn@gmail.com>
2013-09-30 15:34:59 +0200

    trying different neo4jURL

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@
  * GET home page.
  */
 var request = require('superagent');
-var neoURL = process.env.NEO4J_REST_URL || 'http://localhost:7474/db/data';
+var neoURL = process.env.NEO4J_URL  || 'http://localhost:7474';
 
 exports.index = function(req, res) {
-	console.log(neoURL);
+	console.log('url to neo4j rest API' + neoURL);
 	request.post(neoURL + 
-		'/cypher').send({
+		'/db/data/cypher').send({
 			query: 'START n=node(1) RETURN n.title'
 		}).end(function (neo4jRes){
 			res.send(neo4jRes.text);
