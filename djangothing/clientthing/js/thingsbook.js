1670afb70e2aa9f259d42513727e4d614079a3a5
Tor Nilsson Ohrn <bigt@tornet.(none)>
2014-01-22 18:50:36 +0100

    merged with thingscloud from tors github repository

--- djangothing/clientthing/js/thingsbook.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$('.drawing').width($('#globalContainer').width());
-	
-	$(window).resize(function() {
-  	$('.drawing').width($('#globalContainer').width());
-  	// $('.drawing').height('700px');
-  	console.log("rezides");
-	});
-});
